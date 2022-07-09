67c4e7edd9b4de234adf6eedb583bddfd9484e0b
david suran <empty>
2022-07-09 11:32:08 +0200

    Update WebpConverter.sln, Program.cs, and 33 more files...

---
+++ WebpConverter/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using Imazen.WebP;
+
+namespace WebpConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Diagnostics.Debugger.Launch();
+
+            if (args.Length == 0)
+                return;
+
+            try
+            {
+                Imazen.WebP.Extern.LoadLibrary.LoadWebPOrFail();
+
+                foreach(var path in args)
+                {
+                    Convert(path);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+        }
+
+        private static void Convert(string path)
+        {
+            byte[] fileBytes = File.ReadAllBytes(path);
+            string outFile = FormattableString.Invariant($"{Path.GetFileNameWithoutExtension(path)}.jpg");
+            File.Delete(outFile);
+
+            FileStream outStream = new FileStream(outFile, FileMode.Create);
+            SimpleDecoder decoder = new SimpleDecoder();
+
+            Bitmap outBitmap = decoder.DecodeFromBytes(fileBytes, fileBytes.LongLength);
+            outBitmap.Save(outStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+            outStream.Close();
+        }
+    }
+}
